bd5b49086dc92ed41e706258b88e42ce2171ff4d
nick <fengfengb0aw@gmail.com>
2026-02-27 21:55:14 +0800

    Initial Commit

---
+++ world/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  world
+//
+//  Created by nick on 2026/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ world/worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  worldApp.swift
+//  world
+//
+//  Created by nick on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
